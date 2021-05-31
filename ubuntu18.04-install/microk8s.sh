56b7fa1f79ce9d1ab8ed380791a0af0d3a651697
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-06-01 06:57:42 +0900

    Update microk8s.sh

--- ubuntu18.04-install/microk8s.sh
+++ ubuntu18.04-install/microk8s.sh
@@ -19,9 +19,16 @@ microk8s enable dashboard
 microk8s enable ingress
 # microk8s enable metallb 는 별도 입력값(10.0.2.15-10.0.2.15)이 있기 때문에 수작업을 해야 한다.
 
+echo '>>>>> Docker 설치'
+apt install docker.io -y
+echo '{"insecure-registries" : ["localhost:32000"]}' >> /etc/docker/daemon.json
+systemctl restart docker
+
+# root가 아닌 계정에 microk8s 실행 권한 부여
 if [ ! -z $__USER__ ]; then
-	# root가 아닌 계정에 microk8s 실행 권한 부여
 	echo ">>>>> $__USER__에 microk8s 실행 권한 부여 "
 	usermod -a -G microk8s $__USER__
 	chown -f -R $__USER__ ~/.kube
+	
+	usermod -aG docker $__USER__
 fi

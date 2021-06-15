3c6ac7309e25a7f3e6e480a91d23c48c03335252
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-06-16 06:10:12 +0900

    Update microk8s.sh

--- ubuntu18.04-install/microk8s.sh
+++ ubuntu18.04-install/microk8s.sh
@@ -8,6 +8,13 @@ fi
 
 __USER__=$1
 
+echo '>>>>>  cluster hostname 등록'
+cat >> /etc/hosts <<EOF
+172.25.10.136 k8s-1
+172.25.10.103 k8s-2
+172.25.10.56 k8s-3
+EOF
+
 apt install snapd -y
 
 echo '>>>>> MicroK8s 설치 latest/stable 버전'
@@ -29,7 +36,7 @@ microk8s enable registry
 
 echo '>>>>> Docker 설치'
 apt install docker.io -y
-echo '{"insecure-registries" : ["localhost:32000"]}' >> /etc/docker/daemon.json
+echo '{"insecure-registries" : ["k8s-1:32000"]}' >> /etc/docker/daemon.json
 systemctl restart docker
 
 # root가 아닌 계정에 microk8s 실행 권한 부여
@@ -40,3 +47,8 @@ if [ ! -z $__USER__ ]; then
 	
 	usermod -aG docker $__USER__
 fi
+
+# register mirror 등록 및 containerd 재시작
+sed -i.bak -r 's/localhost:32000/k8s-1:32000/g' /var/snap/microk8s/current/args/containerd-template.toml
+microk8s stop
+microk8s start

a276b2ee7f949a1e3f0dcba6d719e40d9ba67237
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-06-22 21:47:40 +0900

    Update postgresql.sh

--- ubuntu18.04-install/postgresql.sh
+++ ubuntu18.04-install/postgresql.sh
@@ -1,2 +1,20 @@
+#!/usr/bin/env bash
 
-apt install postgresql
+if [ -z "$1" ]; then
+	echo ">>>>> usage	: postgresql.sh <db 계정>"
+	echo ">>>>> example	: postgresql.sh unbuntu"
+	exit
+fi
+
+__USER__=$1
+
+# postgresql 리포지토리 및 사이닝키 추가
+echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+apt update -y
+
+# postgresql 12 버전 추가
+apt install postgresql-12 -y
+
+
+

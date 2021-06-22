3093ad187320a48626ee384980aca9b3a92b25aa
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-06-22 22:55:24 +0900

    Update postgresql.sh

--- ubuntu18.04-install/postgresql.sh
+++ ubuntu18.04-install/postgresql.sh
@@ -25,19 +25,18 @@ sed -i.bak -r "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/
 # [변경후] 0.0.0.0:5432            0.0.0.0:*               LISTEN      26412/postgres
 echo "host    all             all             0.0.0.0/0               md5" >> /etc/postgresql/12/main/pg_hba.conf
 
-__USER__=$1
+systemctl restart postgresql
+
 # OS 사용자 생성
 useradd -s /bin/bash -d /home/$__USER__ -m $__USER__
 echo "$__USER__ ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$__USER__
 
-# DB 사용자 및 테이블 생성 : 참고 OS와 DB사용자를 일치시켜라!!!
-su - postgres
-createuser $__USER__
-createdb db_test -O $__USER__
-createdb db_order -O $__USER__
-psql -c "select usename from pg_user;"
-psql -l
-exit
+echo 'DB 사용자 및 테이블 생성은 다음 절차를 따른다. : 참고 OS와 DB사용자를 일치시켜라!!!'
+echo 'su - postgres'
+echo 'createuser __USER__'
+echo 'psql -c "select usename from pg_user;"'
+echo 'createdb db_test -O __USER__'
+echo 'createdb db_order -O __USER__'
+echo 'psql -l'
 
-systemctl restart postgresql
 

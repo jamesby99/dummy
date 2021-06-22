2c8576880a1e45c6c08949a17f98ae7f2890fbe2
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-06-22 22:32:21 +0900

    Update postgresql.sh

--- ubuntu18.04-install/postgresql.sh
+++ ubuntu18.04-install/postgresql.sh
@@ -8,14 +8,24 @@ fi
 
 __USER__=$1
 
-# postgresql 리포지토리 및 사이닝키 추가
+
+# postgresql 리포지토리 및 사이닝키 추가 ------------------------------------------------
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 apt update -y
 
-# postgresql 12 설치
+
+# postgresql 12 설치  -----------------------------------------------------------------
 apt install postgresql-12 -y
 
 
-#listen_addresses = 'localhost'
+# 외부접속 IP 설정 - 접근제한 없이 설정 ------------------------------------------------
+sed -i.bak -r "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/12/main/postgresql.conf
+# [변경전] 127.0.0.1:5432          0.0.0.0:*               LISTEN      24517/postgres
+# [변경후] 0.0.0.0:5432            0.0.0.0:*               LISTEN      26412/postgres
+echo "host    all             all             0.0.0.0/0               md5" >> /etc/postgresql/12/main/pg_hba.conf
+
+su - postgres
+
+systemctl restart postgresql
 

ace3317d0d356dde6636781eddafb1adf6123cbf
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-06-26 12:34:46 +0900

    Update pgpool.sh

--- ubuntu18.04-install/pgpool.sh
+++ ubuntu18.04-install/pgpool.sh
@@ -6,5 +6,5 @@
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 apt update -y
-apt install pgpool2 -y
+apt install pgpool2 postgresql-12-pgpool2 -y
 

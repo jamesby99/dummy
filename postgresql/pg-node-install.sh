237ac393b87cb5522b1b007564dc19ff94bcac71
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-06-27 13:56:14 +0900

    Update pg-node-install.sh

--- postgresql/pg-node-install.sh
+++ postgresql/pg-node-install.sh
@@ -48,7 +48,7 @@ cd ~
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 apt update -y
-apt install postgresql-12 pgpool2 postgresql-12-pgpool2 iputils-arping -y 
+apt install postgresql-12 pgpool2 iputils-arping -y 
 
 sleep 5
 

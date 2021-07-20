cf6322cad6bb8c561d13981b9697f384f77e9a01
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-07-20 20:27:53 +0900

    Update java.sh

--- ubuntu18.04-install/java.sh
+++ ubuntu18.04-install/java.sh
@@ -9,7 +9,7 @@ fi
 __JAVA_VER__=$1
 
 echo ">>>>> jdk $__JAVA_VER__ 설치 "
-apt-get -y install openjdk-$__JAVA_VER__-jdk > /dev/null
+apt-get -y install openjdk-$__JAVA_VER__-jdk
 
 echo '>>>>> JAVA_HOME 설정'
 echo "export JAVA_HOME=/usr/lib/jvm/java-$__JAVA_VER__-openjdk-amd64" >> /etc/profile

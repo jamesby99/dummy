2c0257ecb756ab84c9b4074adc327f6a7cdd4ecd
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-08-14 23:29:41 +0900

    Update hdd-to-ssd.sh

--- postgresql/hdd-to-ssd.sh
+++ postgresql/hdd-to-ssd.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cp -r /postgresql/* /postgresql2
 chown -R postgres:postgres /postgresql2
 sed -i.bak -r "s/random_page_cost = 4/random_page_cost = 1.1/g" /etc/postgresql/11/main/postgresql.conf

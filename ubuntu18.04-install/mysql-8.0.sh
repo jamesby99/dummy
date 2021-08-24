a883569765203df03636b5fc8830172d6310b86c
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-08-24 22:46:13 +0900

    Update mysql-8.0.sh

--- ubuntu18.04-install/mysql-8.0.sh
+++ ubuntu18.04-install/mysql-8.0.sh
@@ -1,2 +1,18 @@
+#!/bin/bash
 
-sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.18-1_all.deb
+# https://awakening95.tistory.com/2
+
+
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.18-1_all.deb
+dpkg -i mysql-apt-config_0.8.18-1_all.deb
+
+# GUI 선택 -> 그냥 OK
+
+apt update -y
+
+apt install mysql-server -y
+# GUI 
+
+mysql_secure_installation
+# interative menu
+

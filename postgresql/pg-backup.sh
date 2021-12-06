117cfb8c31369f618bfbe053053a14ac3c27c219
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-12-06 23:59:57 +0900

    Update pg-backup.sh

--- postgresql/pg-backup.sh
+++ postgresql/pg-backup.sh
@@ -8,27 +8,26 @@ BAK_FILE_NM=_`date +"%Y%m%d"`.tar
 BAK_LOG_FILE_NM=0db_backup_`date +"%Y%m%d"`.log
 BAK_FILE_SAVE_PATH=/root/db-backup/backup-files 
 BAK_FILE_DIRECTORY=`date +"%Y%m%d"` 
-WEEK_AGO=`date -d '1 week ago' +"%Y%m%d"`
+WEEK_AGO=`date -d '3 week ago' +"%Y%m%d"`
 
 source ${PROFILE}
 mkdir ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}
 
 for target in $(cat /root/db-backup/shell-script/backup-db-list.txt); 
 do 
+
   PG_HOST=$(echo $target | cut -d ':' -f1)
   PG_PORT=$(echo $target | cut -d ':' -f2)
   DATABASE=$(echo $target | cut -d ':' -f3)
   PG_USER=$(echo $target | cut -d ':' -f4)
   
-  echo "pg_dump -h ${PG_HOST} -p ${PG_PORT} -d ${DATABASE} -U ${PG_USER} -F t"
-  
   echo `date +"%Y-%m-%d %H:%M:%S"`" @@@ "${DATABASE}" backup shell script start! @@@" >> ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/${BAK_LOG_FILE_NM} 
   
   pg_dump -h ${PG_HOST} -p ${PG_PORT} -d ${DATABASE} -U ${PG_USER} -F t > ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/${DATABASE}${BAK_FILE_NM} 2>&1 && echo `date +"%Y-%m-%d %H:%M:%S"`" @@@ "${DATABASE}" backup shell script end! @@@" >> ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/${BAK_LOG_FILE_NM} 
-  
-  echo "" >> ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/${BAK_LOG_FILE_NM} 
-  
+
   gzip ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/${DATABASE}${BAK_FILE_NM}
+
+  echo "" >> ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/${BAK_LOG_FILE_NM}   
   
 done 
 

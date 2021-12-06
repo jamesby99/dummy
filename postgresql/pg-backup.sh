6f4d174570c9bf0c5359924e33695d4800f27761
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-12-06 23:29:57 +0900

    Update pg-backup.sh

--- postgresql/pg-backup.sh
+++ postgresql/pg-backup.sh
@@ -2,7 +2,6 @@
 
 # 편집필요: 사용 변수
 PROFILE='/root/.profile'
-PG_HOST='pg-vip'
 PG_PORT=8282
 
 # 변수
@@ -15,11 +14,16 @@ WEEK_AGO=`date -d '1 week ago' +"%Y%m%d"`
 source ${PROFILE}
 mkdir ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}
 
-for backup_database in $(cat /root/db-backup/shell-script/backup-db-list.txt); 
+for target in $(cat /root/db-backup/shell-script/backup-db-list.txt); 
 do 
+  pg_host=$(echo $target | cut -d ':' -f1)
+  backup_database=$(echo $target | cut -d ':' -f2)
+  echo $pg_host
+  echo $backup_database
+  
   echo `date +"%Y-%m-%d %H:%M:%S"`" @@@ "$backup_database" backup shell script start! @@@" >> ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/${BAK_LOG_FILE_NM} 
   
-  pg_dump -h ${PG_HOST} -p ${PG_PORT} -d "$backup_database" -U postgres -F t > ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/$backup_database${BAK_FILE_NM} 2>&1 && echo `date +"%Y-%m-%d %H:%M:%S"`" @@@ "$backup_database" backup shell script end! @@@" >> ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/${BAK_LOG_FILE_NM} 
+  pg_dump -h ${pg_host} -p ${PG_PORT} -d "$backup_database" -U postgres -F t > ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/$backup_database${BAK_FILE_NM} 2>&1 && echo `date +"%Y-%m-%d %H:%M:%S"`" @@@ "$backup_database" backup shell script end! @@@" >> ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/${BAK_LOG_FILE_NM} 
   
   echo "" >> ${BAK_FILE_SAVE_PATH}/${BAK_FILE_DIRECTORY}/${BAK_LOG_FILE_NM} 
   

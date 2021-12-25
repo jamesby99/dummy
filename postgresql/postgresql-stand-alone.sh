4af55a9c00c39380f25795808ee7a3b15224b4c9
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-12-26 01:06:02 +0900

    Create postgresql-stand-alone.sh

---
+++ postgresql/postgresql-stand-alone.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+apt update -y
+apt install postgresql-11 -y

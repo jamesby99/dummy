5f947adb67db8db488d004cc559587f8d2b76bed
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-12-26 01:09:09 +0900

    Update postgresql-stand-alone.sh

--- postgresql/postgresql-stand-alone.sh
+++ postgresql/postgresql-stand-alone.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -

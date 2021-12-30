c136656f264051b253367e468f89842b5ae1f160
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2021-12-30 15:54:01 +0900

    Create mysql8.0-dpkg-noninteractive-full-delete.sh

---
+++ mysql/mysql8.0-dpkg-noninteractive-full-delete.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# [ 실행전 TODO, 꼭 확인할 것 ]
+#
+# 1. root 계정에서 실행 할 것
+# 2. 정말 이전 버전의 설정, 데이타, 로그 등을 모두 삭제할 것인?
+# 3. 
+#------------------------------------------------------------------------------
+
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ; then
+	echo ">>>>> usage	: mysql8.0-dpkg-noninteractive-full-delete.sh"
+	echo ">>>>> example	: mysql8.0-dpkg-noninteractive-full-delete.sh"
+	exit
+fi
+
+
+# noninteractive 설정 - 전체 완전 삭제
+debconf-set-selections <<< "mysql-community-server mysql-community-server/remove-data-dir boolean true"
+DEBIAN_FRONTEND=noninteractive
+
+# 완전삭제 수행
+dpkg -P mysql-common mysql-community-client-plugins mysql-community-client-core mysql-community-client mysql-client mysql-community-server-core mysql-community-server mysql-server

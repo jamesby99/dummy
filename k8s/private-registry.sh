7ee299bd2644535ca30f518688f16b66598be0fb
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2022-01-21 13:50:01 +0900

    Update private-registry.sh

--- k8s/private-registry.sh
+++ k8s/private-registry.sh
@@ -38,7 +38,7 @@ echo 'alias h=microk8s.helm3' >> /etc/profile
 echo 'alias helm=microk8s.helm3' >> /etc/profile
 
 # 필요한 애드온들 활성화
-echo '>>>>>  dns, dashboard, helm3, storage, registry 애드온 활성화'
+echo '>>>>>  dns, registry 애드온 활성화'
 microk8s enable dns
 microk8s enable registry
 

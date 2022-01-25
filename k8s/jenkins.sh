ec8e952eab46a488eee826a8271bb169020dcae3
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2022-01-25 21:37:17 +0900

    Create jenkins.sh

---
+++ k8s/jenkins.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# apt lock 사전 제거
+killall apt apt-get
+rm /var/lib/apt/lists/lock
+rm /var/cache/apt/archives/lock
+rm /var/lib/dpkg/lock
+
+apt update
+apt -y upgrade
+
+# TIME-ZONE(Asia/Seoul) 설정
+timedatectl set-timezone Asia/Seoul
+
+# java11
+apt-get -y install openjdk-11-jdk
+apt-get -y install openjdk-8-jdk
+
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/profile
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+source /etc/profile
+
+
+# Jenkins 설치를 위해 Repository key 추가
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+
+#  서버의 sources.list에 Jenkins 패키지 저장소를 추가
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+# 패키지 인덱스 정보 업데이트
+sudo apt-get update -y
+
+# Jenkins 패키지 설치
+sudo apt-get install jenkins  -y

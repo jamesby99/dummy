62223b343f410daa13f7d1a875a7bab506c3b386
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2022-03-15 05:00:44 +0900

    Create java.sh

---
+++ dbaas/java.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+	echo ">>>>> usage	: java.sh <java version>"
+	echo ">>>>> example	: java.sh 11"
+	exit
+fi
+
+__JAVA_VER__=$1
+
+echo ">>>>> jdk $__JAVA_VER__ 설치 "
+apt-get -y install openjdk-$__JAVA_VER__-jdk
+
+echo '>>>>> JAVA_HOME 설정'
+echo "export JAVA_HOME=/usr/lib/jvm/java-$__JAVA_VER__-openjdk-amd64" >> /etc/profile
+export JAVA_HOME=/usr/lib/jvm/java-$__JAVA_VER__-openjdk-amd64
+source /etc/profile

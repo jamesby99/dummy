ccef6386d99b3823b80a705a5def489b5b1e53ce
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2022-03-24 22:43:33 +0900

    Update pg-node-install-for-gcloud-2x2.sh

--- postgresql/pg-node-install-for-gcloud-2x2.sh
+++ postgresql/pg-node-install-for-gcloud-2x2.sh
@@ -274,7 +274,7 @@ local   all             all                                     trust
 host    all             all             127.0.0.1/32            trust
 # IPv6 local connections:
 host    all             all             ::1/128                 trust
-host    all             all             0.0.0.0/32              trust
+host    all             all             0.0.0.0/0               trust
 EOF
 
 

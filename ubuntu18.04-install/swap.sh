f361dd0637745c22ff104a2c9b2ea22f75ffc8a1
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2022-10-02 16:06:01 +0900

    Update swap.sh

--- ubuntu18.04-install/swap.sh
+++ ubuntu18.04-install/swap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sudo swapoff -v /swap.img
-sudo rm -rf /swap.img
+swapoff -v /swap.img
+rm -rf /swap.img
 
 fallocate -l 8G /swap.img
 chmod 600 /swap.img
 mkswap /swap.img
 swapon /swap.img
+swapon -show

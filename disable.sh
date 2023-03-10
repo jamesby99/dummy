d9a6e0579ee2e6e791e9c673c72f8e35811cbb82
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2023-03-10 13:29:36 +0900

    Create disable.sh

---
+++ disable.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo [[ Check APT Timer ]]
+result=`systemctl list-timers | grep apt-daily`
+echo $result
+if [[ "$result" == *apt-daily.service* ]]; then
+  echo Find APT Timer T.T
+  sudo systemctl stop apt-daily.timer
+  sudo systemctl disable apt-daily.timer
+  sudo systemctl disable apt-daily.service
+  sudo systemctl stop apt-daily-upgrade.timer
+  sudo systemctl disable apt-daily-upgrade.timer
+  sudo systemctl disable apt-daily-upgrade.service
+  sudo systemctl daemon-reload
+else
+  echo No APT Timer!! ^^
+fi
+echo -----------------------
+echo "\n"
+echo [[ print ip a ]]
+sudo ip a
+echo --- Check Again VIP ---
+return

aaa8c60ddf3ddc33e2412ce7b4045274b2035f3c
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2023-05-09 13:47:27 +0900

    Update cloud-init.sh

--- ubuntu18.04-install/cloud-init.sh
+++ ubuntu18.04-install/cloud-init.sh
@@ -5,6 +5,8 @@ sudo rm /var/lib/apt/lists/lock
 sudo rm /var/cache/apt/archives/lock
 sudo rm /var/lib/dpkg/lock
 
+sed -i.bak -r 's/([a-z]{2}.)?(archive|security).ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+
 sudo apt -y update
 sudo apt -y upgrade
 

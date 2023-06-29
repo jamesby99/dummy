eab8dec88cba2f3ac7b8c157cd745e6447fcaef0
jamesby99 <63693520+jamesby99@users.noreply.github.com>
2023-06-29 11:42:11 +0900

    Update temurin-jdk.sh

--- ubuntu18.04-install/temurin-jdk.sh
+++ ubuntu18.04-install/temurin-jdk.sh
@@ -11,7 +11,7 @@ __JAVA_VER__=$1
 echo ">>>>> jdk $__JAVA_VER__ 설치 "
 sudo apt install -y wget apt-transport-https
 sudo mkdir -p /etc/apt/keyrings
-sudo wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
+sudo wget --no-check-certificate -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
 sudo echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
 sudo apt update
 sudo apt install temurin-$__JAVA_VER__-jdk
